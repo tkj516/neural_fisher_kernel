88d2ba8997a19bf287839981399a0a4d5432c1e0
Maohao Shen <maohao@maohaos-mbp.usma.ibm.com>
2024-03-15 11:46:39 -0400

    fix the energy_net

--- models/energy_net.py
+++ models/energy_net.py
@@ -7,6 +7,7 @@ MODEL_MAPPING = {
 'MobileNet': MobileNetV2,
 # define other model mapping here
 }
+'''
 class EnergyNet(nn.Module):
     def __init__(self, model_name, **kwargs):
         super(EnergyNet, self).__init__()
@@ -21,10 +22,21 @@ class EnergyNet(nn.Module):
         out = self.base_model.forward(x)
         probs = F.softmax(out, dim=-1)
         return torch.sum(probs.detach() * out, dim=-1)
+'''
+class EnergyNet(nn.Module):
+    def __init__(self, base_model = MobileNetV2, **kwargs):
+        base_model.__init__(self, **kwargs)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Returns \sum_{y \in Y} p(y | x) f^y(x).
+        """
+        out = base_model.forward(x)
+        probs = F.softmax(out, dim=-1)
+        return torch.sum(probs.detach() * out, dim=-1)
 
 def energynet(model_name, model_path, pretrained=False, device="cpu", **kwargs):
-    model = EnergyNet(model_name, **kwargs)
+    model = EnergyNet(MODEL_MAPPING[model_name], **kwargs)
     if pretrained:
         state_dict = torch.load(model_path, map_location=device)
-        model.base_model.load_state_dict(state_dict)
+        model.load_state_dict(state_dict)
     return model
