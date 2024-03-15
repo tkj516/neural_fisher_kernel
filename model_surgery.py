3d801684437e3270dc142cb5e74ea5f1506b93b4
Tejas Jayashankar <tejaskj@gmail.com>
2024-03-15 12:32:38 -0400

    Fixes to model surgery

--- model_surgery.py
+++ model_surgery.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Dict
 
 import torch
@@ -8,8 +9,12 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.datasets import CIFAR10
 from tqdm import tqdm
+from zmq import has
 
 from mobilenetv2 import MobileNetV2
+from utils import clip_grad_norm
+
+import copy
 
 BATCH_SIZE = 32
 LEARNING_RATE_CLASSIFIER = 0.001
@@ -17,6 +22,10 @@ LEARNING_RATE_EPS = 0.01
 DATA_DIR = "/home/tejasj/data"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TOTAL_ITERS = 10_000
+
+
+def move_to_device(item, device):
+    return (item[0], item[1].to(device))
 
 
 class SurgerizedModel(MobileNetV2):
@@ -33,13 +42,37 @@ class SurgerizedModel(MobileNetV2):
 
         self.eps = nn.Parameter(torch.rand(num_basis, requires_grad=True))
 
+    def cache_state_dict(self):
+        if hasattr(self, "cached_state_dict"):
+            raise ValueError("Cached state dict already exists.")
+        
+        self.cached_state_dict = [
+            copy.deepcopy(
+                dict(
+                    map(
+                        functools.partial(move_to_device, device="cpu"),
+                        self.state_dict().items(),
+                    )
+                )
+            )
+        ]
+
     def update_weights(self, basis: Dict[str, torch.Tensor]) -> None:
+        if not hasattr(self, "cached_state_dict"):
+            raise ValueError(
+                "Cached state dict not found. Call cache_state_dict first."
+            )
+        
+        cached_state_dict = self.cached_state_dict[0]
         new_state_dict = self.state_dict()
         for key, value in basis.items():
             if key in self.state_dict():
-                new_state_dict[key] = torch.einsum(
+                new_state_dict[key] = cached_state_dict[
+                    key
+                ].detach().cpu() + torch.einsum(
                     "l...,l->...", value, self.eps.detach().cpu()
                 )
+        # Store the current state dict to use in the next iteration
         self.load_state_dict(new_state_dict)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -47,6 +80,21 @@ class SurgerizedModel(MobileNetV2):
         x = x.mean([2, 3])
         x = self.new_classifier(x)
         return x
+
+
+def surgerized(pretrained=False, device="cpu", **kwargs):
+    model = SurgerizedModel(**kwargs)
+    if pretrained:
+        state_dict = torch.load(
+            "/home/tejasj/data2/PyTorch_CIFAR10/cifar10_models/state_dicts/mobilenet_v2.pt",
+            map_location=device,
+        )
+        pretrained_state_dict = {
+            k: v for k, v in state_dict.items() if k in model.state_dict()
+        }
+        state_dict.update(pretrained_state_dict)
+        model.load_state_dict(state_dict)
+    return model
 
 
 if __name__ == "__main__":
@@ -72,7 +120,9 @@ if __name__ == "__main__":
         pin_memory=True,
     )
 
-    model = SurgerizedModel(num_basis=20)
+    model = surgerized(num_basis=20)
+    # Cache state dict for safekeeping
+    model.cache_state_dict()
 
     optimizer_classifier = torch.optim.AdamW(
         model.new_classifier.parameters(), lr=LEARNING_RATE_CLASSIFIER
@@ -93,6 +143,7 @@ if __name__ == "__main__":
             out = model(inputs)
             loss = F.cross_entropy(out, labels)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 100.0)
 
             eps_grad = 0
             for key, param in model.named_parameters():
@@ -104,8 +155,8 @@ if __name__ == "__main__":
             model.eps.data = model.eps.data - LEARNING_RATE_EPS * eps_grad.to(
                 model.eps.device
             )
-            optimizer_classifier.step()
 
+            optimizer_classifier.step()
             model.update_weights(basis)
 
             if step % 50 == 0:
